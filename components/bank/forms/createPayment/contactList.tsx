ab1efabdc682a7cd63837dede68d1b68d4da8000
Philip Mattha <philip@mattha.net>
2025-09-24 10:18:28 +0200

    feat: Add empty state for beneficiary filtering

--- components/bank/forms/createPayment/contactList.tsx
+++ components/bank/forms/createPayment/contactList.tsx
@@ -2,8 +2,9 @@ import { Head } from "fresh/runtime";
 import { Contact } from "@/services/contacts.ts";
 import { Card } from "@/components/card.tsx";
 import { countryFlags } from "@/data/countries.ts";
-import { Button } from "@/components/Button.tsx";
+import { Button, LinkButton } from "@/components/Button.tsx";
 import { ArrowRight } from "lucide-preact";
+import Message from "../../../message.tsx";
 
 export function ContactList(
   { contacts, search }: { contacts: Contact[]; search?: string },
@@ -17,49 +18,46 @@ export function ContactList(
         Beneficiary
       </h1>
       <Card>
-        <div class="flex flex-row justify-between border-b border-gray-200 pl-12 py-8 pr-6">
-          <form action="" method="GET">
-            <input
-              type="text"
-              accessKey="f"
-              placeholder="Search beneficiery..."
-              class="border rounded-sm border-gray-400 px-2 py-1"
-              name="search"
-              value={search ?? ""}
-            />
-          </form>
-          <a>New beneficiary</a>
-        </div>
-        <form action="" method="POST" id="beneficiary">
-          <ul>
-            {contacts.map((item) => (
-              <li class="relative not-last:border-b border-gray-200 hover:bg-gray-100 has-focus:bg-blue-600/10 ">
-                <label class="flex flex-col px-6 pl-12 py-4 cursor-pointer">
-                  <span class="col-span-3 font-medium text-lg">
-                    {item.contact_label}
-                  </span>
-                  <span class="flex flex-row gap-4 text-gray-700 *:not-last:after:content-['•'] *:not-last:after:pl-3 *:not-last:after:text-gray-400">
-                    <span>{item.account_number}</span>
-                    <span>{item.currency}</span>
-                    <span>
-                      {countryFlags[
-                        item.country.toLowerCase() as keyof typeof countryFlags
-                      ]
-                        .name}
-                    </span>
-                  </span>
-                  <input
-                    required
-                    type="radio"
-                    name="beneficiary"
-                    value={item.id}
-                    class="absolute top-9 left-4 opacity-0 checked:opacity-100"
-                  />
-                </label>
-              </li>
-            ))}
-          </ul>
-        </form>
+        <Search search={search} />
+        {contacts.length === 0
+          ? (
+            <Message title="No items found" className="px-24 py-16">
+              <LinkButton href="./newPayment/">Remove filter</LinkButton>
+            </Message>
+          )
+          : (
+            <form action="" method="POST" id="beneficiary">
+              <ul>
+                {contacts.map((item) => (
+                  <li class="relative not-last:border-b border-gray-200 hover:bg-gray-100 has-focus:bg-blue-600/10 ">
+                    <label class="flex flex-col px-6 pl-12 py-4 cursor-pointer">
+                      <span class="col-span-3 font-medium text-lg">
+                        {item.contact_label}
+                      </span>
+                      <span class="flex flex-row gap-4 text-gray-700 *:not-last:after:content-['•'] *:not-last:after:pl-3 *:not-last:after:text-gray-400">
+                        <span>{item.account_number}</span>
+                        <span>{item.currency}</span>
+                        <span>
+                          {countryFlags[
+                            item.country
+                              .toLowerCase() as keyof typeof countryFlags
+                          ]
+                            .name}
+                        </span>
+                      </span>
+                      <input
+                        required
+                        type="radio"
+                        name="beneficiary"
+                        value={item.id}
+                        class="absolute top-9 left-4 opacity-0 checked:opacity-100"
+                      />
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </form>
+          )}
       </Card>
       <div class="pt-4 flex flex-row justify-end">
         <Button type="submit" form="beneficiary">
@@ -68,5 +66,23 @@ export function ContactList(
         </Button>
       </div>
     </main>
+  );
+}
+
+function Search({ search }: { search: string | undefined }) {
+  return (
+    <div class="flex flex-row justify-between border-b border-gray-200 pl-12 py-8 pr-6">
+      <form action="" method="GET">
+        <input
+          type="text"
+          accessKey="f"
+          placeholder="Search beneficiery..."
+          class="border rounded-sm border-gray-400 px-2 py-1"
+          name="search"
+          value={search}
+        />
+      </form>
+      <a>New beneficiary</a>
+    </div>
   );
 }
--- components/message.tsx
+++ components/message.tsx
@@ -1,14 +1,18 @@
 import { ComponentChildren } from "preact";
+import { cn } from "../utils/utils.ts";
 
 export default function Message(
-  { title, subline, children }: {
+  { title, subline, children, className }: {
     title?: string;
     subline?: string;
     children?: ComponentChildren;
+    className?: string;
   },
 ) {
   return (
-    <div class="grow flex flex-col items-center justify-center">
+    <div
+      class={cn("grow flex flex-col items-center justify-center", className)}
+    >
       <h3>{title}</h3>
       <p>{subline}</p>
       <div>
