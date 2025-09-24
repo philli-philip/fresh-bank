0f2402b75a4cf67b3132ec4ad5c13a045d2861f2
Philip Mattha <philip@mattha.net>
2025-09-24 21:39:24 +0200

    feat: beatutify summary page

--- components/bank/forms/createPayment/summaryPage.tsx
+++ components/bank/forms/createPayment/summaryPage.tsx
@@ -1,15 +1,40 @@
 import { Button } from "@/components/Button.tsx";
 import { Card } from "@/components/card.tsx";
 import { Head } from "fresh/runtime";
+import { AlertTriangle } from "lucide-preact";
+
+export function renderAmount(
+  cent: number,
+  currency: string,
+  locale?: string,
+  showCurreny?: boolean,
+) {
+  const formatter = new Intl.NumberFormat(locale ?? "de-DE", {
+    style: "currency",
+    minimumFractionDigits: 2,
+    currency: currency,
+    currencyDisplay: showCurreny ?? true ? "code" : undefined,
+  });
+
+  return formatter.format(cent);
+}
 
 export function SummaryPage(
   { summary, id }: {
     id: string;
     summary: {
-      contact_label?: string;
-      account_owner: string;
+      process: number;
       amount: number;
+      reference_text?: string;
+      account_owner: string;
+      contact_label?: string;
+      account_number: string;
+      bank: string;
       currency: string;
+      town?: string;
+      country?: string;
+      eMail?: null;
+      street?: null;
     };
   },
 ) {
@@ -21,19 +46,37 @@ export function SummaryPage(
         </title>
       </Head>
       <input hidden name="process" value={id} />
-      <Card className="px-12 py-6">
-        Summary
-        <div>
-          <span>Receipient</span>
-          <span>
-            {summary.contact_label ??
-              summary.account_owner}
+      <Card className="px-12 py-6 flex flex-col gap-2 mb-6">
+        <h1 class="font-semibold text-lg col-span-3">Summary</h1>
+        <div class="grid grid-cols-[1fr_3fr] gap-2 pb-8">
+          <span class="row-span-3">Receipient</span>
+          <span class="font-semibold">
+            {summary.contact_label ?? summary.account_owner}
           </span>
-          <span>{summary.amount}</span>
-          <span>{summary.currency}</span>
+          <span class="">{summary.bank}</span>
+          <span class="">{summary.account_number}</span>
+        </div>
+        <div class="grid grid-cols-[1fr_3fr] gap-2 pb-8">
+          <span class="">Amount</span>
+          <span>{renderAmount(summary.amount, summary.currency)}</span>
+          <span class="">Reference</span>
+          <span>{summary.reference_text}</span>
+          <span class="">Execution</span>
+          <span>Instantly after authorisation</span>
         </div>
       </Card>
-      <div class="flex flex-row pt-4 justify-end">
+      <Card className="px-12 py-8 mb-6 bg-blue-100 flex gap-4 flex-row">
+        <AlertTriangle size="32" class="-ml-2" />
+        <div class="flex flex-col gap-2">
+          <h3 class="font-semibold text-lg">Approval needed</h3>
+          <p class="max-w-xl">
+            Your team uses a 4 eye principle for all payments. This means that
+            another person from your authorisation group will need to approve
+            the payment.
+          </p>
+        </div>
+      </Card>
+      <div class="flex flex-row justify-end">
         <Button type="submit">
           Submit payment
         </Button>
