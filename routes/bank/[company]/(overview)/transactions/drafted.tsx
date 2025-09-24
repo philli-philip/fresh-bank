3294e03bbf70431bee59bcede251afba95b52143
Philip Mattha <philip@mattha.net>
2025-09-24 15:57:48 +0200

    feat: improve draft list

--- routes/bank/[company]/(overview)/transactions/drafted.tsx
+++ routes/bank/[company]/(overview)/transactions/drafted.tsx
@@ -9,6 +9,13 @@ import {
 import { MoreVertical, Plus, Trash } from "lucide-preact";
 import { LinkButton } from "@/components/Button.tsx";
 import { PageHeader } from "@/components/bank/pageHeader.tsx";
+import Message from "@/components/message.tsx";
+
+const amountFormatter = Intl.NumberFormat("de-DE", {
+  minimumFractionDigits: 2,
+  style: "currency",
+  currency: "EUR",
+});
 
 export const handler = define.handlers({
   async POST(ctx) {
@@ -33,7 +40,16 @@ export const handler = define.handlers({
 });
 
 export default define.page(() => {
-  const drafted = db.prepare("SELECT * FROM draft_payments").all() as {
+  const drafted = db.prepare(`
+    SELECT 
+      draft_payments.id,
+      amount,
+      currency,
+      COALESCE(contact_label, account_owner) as beneficiary 
+    FROM draft_payments
+    LEFT JOIN contacts ON contacts.id = draft_payments.beneficiary_id
+
+    `).all() as {
     id: string;
     amount?: number;
     beneficiary?: string;
@@ -46,12 +62,29 @@ export default define.page(() => {
           New payment
         </LinkButton>
       </PageHeader>
-      <Card className="overflow-hidden">
+      <Card>
+        {drafted.length === 0 && (
+          <Message
+            className="px-24 py-16"
+            title="No drafted payments"
+            subline="Payments are automatically saved during creation"
+          >
+            <LinkButton href="/bank/all/newPayment/">
+              <Plus size="16" />Create Payment
+            </LinkButton>
+          </Message>
+        )}
         <ul>
           {drafted.map((item) => (
-            <li class="grid isolate hover:bg-gray-100 duration-75 grid-cols-2 items-center px-4 py-3 justify-between not-last:border-b border-gray-200 relative">
-              {item.id}
-              <Dropdown className="place-self-end">
+            <li class="hover:bg-gray-100 duration-75 grid grid-cols-4 items-center px-4 py-3 justify-between not-last:border-b border-gray-200 relative">
+              <span class="font-semibold">{item.id}</span>
+              {item.amount
+                ? <span>{amountFormatter.format(item.amount / 100)}</span>
+                : <span class="text-gray-500">Not set</span>}
+              {item.beneficiary
+                ? <span>{item.beneficiary}</span>
+                : <span class="text-gray-500">Not set</span>}
+              <Dropdown className="place-self-end z-10">
                 <button
                   type="button"
                   class="p-2 rounded-sm hover:bg-gray-200 duration-75 cursor-pointer"
@@ -74,15 +107,10 @@ export default define.page(() => {
                   </DropdownItem>
                 </DropdownMenu>
               </Dropdown>
-              <form method="POST">
-                <input hidden name="action" value="edit" />
-                <input hidden name="id" value={item.id} />
-                <button
-                  type="submit"
-                  class="absolute inset-0 z-10 cursor-pointer"
-                  aria-label="continue drafted payment"
-                />
-              </form>
+              <a
+                href={`/bank/all/newPayment/${item.id}/amount/`}
+                class="absolute inset-0"
+              />
             </li>
           ))}
         </ul>
